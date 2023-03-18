2707042bf3bc1473ee00935830b0ce2c9fe5bf9a
Olya Vo <54139547+do-your-own-seo@users.noreply.github.com>
2023-03-19 02:15:35 +0300

    Create gtag-init.js

---
+++ gtag-init.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'analytics_storage': 'denied'
+});
+
+gtag('config', 'G-YOUR_ID');
